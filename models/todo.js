aeccbc6d0bce033c4025fdb022f4f47b7ce82be0
Alexsi <Alexsi@email.com>
2017-01-02 21:26:53 +0100

    Metodo Update PUT /todos/:id con l'uso di sequelize

--- models/todo.js
+++ models/todo.js
@@ -1,6 +1,8 @@
 /**
  * Created by Giuseppe on 02/01/2017.
  */
+var _ = require('underscore');
+
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define('todo', {
         description :{
@@ -15,5 +17,13 @@ module.exports = function (sequelize, DataTypes) {
             allowNull: false,
             defaultValue: false
         }
-    });
+    }, {
+            validate:{
+                descriptionIsString: function () {
+                    if(!_.isString(this.description)){
+                      throw new Error('La descrizione deve essere uan stinga.')
+                    }
+                }
+            }
+        });
 };
