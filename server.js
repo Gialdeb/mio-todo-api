fea6ab6f119059f2d17b86349ebe9f2fcf1f66bd
Alexsi <Alexsi@email.com>
2017-01-02 16:51:15 +0100

    Aggiungo il model per il database nel metodo POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -55,7 +56,16 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed'); // Use _.pick description and completed
 
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+    db.todo.create(body).then(function (todo) {
+       res.json(todo.toJSON())
+    }, function (e) {
+        res.status(400).json(e);
+    });
+    // call create on db.todo
+    // respond with 200 and todo
+    // e resp.status(400).json(e)
+
+   /* if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
         return res.status(404).send();
     }
 
@@ -72,7 +82,7 @@ app.post('/todos', function (req, res) {
 
 
 
-    res.json(body);
+    res.json(body);*/
 });
 
     // DELETE /todos/:id
@@ -146,7 +156,11 @@ app.put('/todos/:id', function (req, res) {
 });
 
 
-app.listen(PORT, function () {
-    console.log('Express e partito sulla porta ' + PORT + '!' );
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log('Express e partito sulla porta ' + PORT + '!' );
+    });
 });
+
+
 
