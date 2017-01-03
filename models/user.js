968e460bf8596874683ab26974787478dd58f794
Alexsi <Alexsi@email.com>
2017-01-03 18:05:53 +0100

    Aggiunto il token alla richiesta di autenticazione POST /users/login

--- models/user.js
+++ models/user.js
@@ -5,6 +5,8 @@
 
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function (sequelize, DataTypes) {
     var user = sequelize.define('user', {
@@ -74,6 +76,22 @@ module.exports = function (sequelize, DataTypes) {
             toPublicJSON: function () {
                 var json = this.toJSON();
                 return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+            },
+            generateToken: function (type) {
+                if(!_.isString(type)){
+                    return undefined;
+                }
+                try {
+                    var stringData = JSON.stringify({id: this.get('id'), type: type});
+                    var encrypData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
+                    var token = jwt.sign({
+                        token: encrypData
+                    }, 'qwerty098');
+                    return token;
+                } catch (e) {
+                    console.error(e);
+                    return undefined;
+                }
             }
         }
         
